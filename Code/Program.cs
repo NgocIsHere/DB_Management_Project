daee000488e3dad43cc9516f44d225351ca26d8b
NgocIsHere <111295106+NgocIsHere@users.noreply.github.com>
2024-06-01 20:10:14 +0700

    commit to pull

--- Code/Program.cs
+++ Code/Program.cs
@@ -17,11 +17,8 @@ namespace DB_Management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
             Application.Run(new Login());
-=======
-            Application.Run(new PH2());
->>>>>>> d6bed82f54eaaad3b33343defc57b7fae40efde5
+            // Application.Run(new PH2());
         }
     }
 }
